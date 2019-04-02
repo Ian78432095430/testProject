873edc465ddbf8a2d30672ac3f8dea45d244c27a
Ian Starks <istarks2022@208-Engineering-7.local>
2019-04-02 09:09:15 -0700

    Initial Commit

---
+++ testProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testProject
+//
+//  Created by Ian Starks on 4/2/19.
+//  Copyright © 2019 Ian Starks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
